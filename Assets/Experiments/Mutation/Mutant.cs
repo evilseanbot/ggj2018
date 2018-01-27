4045b05b51e644970a05f3e5df87ab73e7ca1754
Seanna Musgrave <seannamusgrave@gmail.com>
2018-01-27 00:35:21 -0800

    Added some sweet assets

---
+++ Assets/Experiments/Mutation/Mutant.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mutant : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<Heartbeat> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Experiments/Mutation/WorldMutation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMutation : MonoBehaviour {
+
+	public static WorldMutation instance;
+	public WorldHeartbeat heartbeat;
+	public WorldShiftingTexture shiftingTexture;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		heartbeat = gameObject.AddComponent<WorldHeartbeat> ();
+		shiftingTexture = gameObject.AddComponent<WorldShiftingTexture> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
