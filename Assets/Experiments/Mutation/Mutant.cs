fe638e796ee861fc7c6ac54cebb0b63c37aba9dc
Seanna Musgrave <seannamusgrave@gmail.com>
2018-01-28 14:16:10 -0800

    Re-added changes to experiments

--- Assets/Experiments/Mutation/Mutant.cs
+++ Assets/Experiments/Mutation/Mutant.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mutant : MonoBehaviour {
 	public bool dimensional = false;
 	public Dimension dimension = Dimension.none;
+	public bool prop = false;
 
 	public enum Dimension {A, B, C, none};
 
@@ -12,28 +13,31 @@ public class Mutant : MonoBehaviour {
 	void Start () {
 		WorldMutation.OnScrubbed += OnScrubbed;
 		if (dimensional) {
-			WorldMutation.OnDimensionalEnter += OnDimensionalEnter;
-			WorldMutation.OnDimensionalExit += OnDimensionalExit;
+			WorldDimension.OnDimensionalEnter += OnDimensionalEnter;
+			WorldDimension.OnDimensionalExit += OnDimensionalExit;
 		}
 	}
-	
-	void OnScrubbed() {
-		Debug.Log ("A scrub has happened");
 
+	void OnScrubbed() {
+		//Debug.Log ("A scrub has happened");
+	}
+
+	void Update() {
 		//Heartbeat effect
-		transform.localScale = Vector3.one * WorldMutation.instance.heartbeat.scaleMod;
+		if (prop) {
+			transform.localScale = Vector3.one * WorldMutation.instance.heartbeat.scaleMod;
+		}
 	}
 
 	void OnDimensionalEnter(Dimension dimensionEntered) {
-		Debug.Log("On dimensional enter");
-		Debug.Log (dimensionEntered);
+		//Debug.Log("On dimensional enter");
 		if (dimension == dimensionEntered) {
 			GetComponent<Renderer> ().enabled = true;
 		}
 	}
 
 	void OnDimensionalExit(Dimension dimensionExited) {
-		Debug.Log("On dimensional exit");
+		//Debug.Log("On dimensional exit");
 		if (dimension == dimensionExited) {
 			GetComponent<Renderer> ().enabled = false;
 		}
